f4c139f414dd3addbcce76a538fdd30f8ceedb3f
Bill Molchan <11306048+Divine-Shadow@users.noreply.github.com>
2026-05-26 17:10:29 -0400

    Preserve ouro integration runtime inputs

--- overlays/ouroboros-terraform/runtime.nix
+++ overlays/ouroboros-terraform/runtime.nix
@@ -7,5 +7,9 @@ mkShell "ouroboros-terraform" (toolsets.ouroborosAgentTools ++ [
   export JAVA_HOME=${pkgs.jdk21}
   export GOROOT=${packages.pinnedGo}
   export NODE_PATH=${pkgsPlaywright.playwright-test}/lib/node_modules:${packages.pinnedNpmTools}/lib/devkit-npm-tools/node_modules''${NODE_PATH:+:$NODE_PATH}
+  export AWS_CONFIG_FILE="$HOME/.aws/config"
+  export AWS_SHARED_CREDENTIALS_FILE="$HOME/.aws/credentials"
+  export AWS_SDK_LOAD_CONFIG=1
+  export AWS_PROFILE="''${AWS_PROFILE:-ouroboros}"
   export PATH=${pkgs.jdk21}/bin:$PATH
 ''
