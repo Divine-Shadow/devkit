70fa7831fa73dbc9820417bea3eb4ecc0478db72
Bill Molchan <11306048+Divine-Shadow@users.noreply.github.com>
2026-07-01 19:04:33 -0400

    devkit: pin governance schema runtime

--- flake.nix
+++ flake.nix
@@ -57,7 +57,7 @@
       };
       codexVersion = "0.142.5";
       codexReleaseTag = "rust-v${codexVersion}";
-      governanceJarVersion = "b3f081db035cfab4a3ee699a353d5f0e35e8ff62";
+      governanceJarVersion = "a580b040538328a59fee533d034a624505e2de9c";
       submitRuntimeVersion = "a95db2da7c7f1f565918f726f962c3e779031e27";
       sbtControlPlaneRuntimeVersion = "be9a16cd8abdf9d479bbf0b7379ebdf0651d156e";
       governanceJarSourceFlake = builtins.getFlake "git+file:///workspaces/dev/ouroboros-ide?rev=${governanceJarVersion}";
